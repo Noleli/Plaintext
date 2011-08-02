bd382877f5c1d69a5a06de14e6cb90dbe3a43af5
Noah Liebman <noah@noahliebman.com>
2011-08-01 23:15:48 -0500

    feeds

--- header.php
+++ header.php
@@ -27,6 +27,12 @@
 <link href="<?php bloginfo('template_url'); ?>/reset.css" rel="stylesheet" type="text/css" />
 <link href="<?php bloginfo('template_url'); ?>/960.css" rel="stylesheet" type="text/css" />
 <link href="<?php bloginfo('template_url'); ?>/style.css" rel="stylesheet" type="text/css" />
+
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+
+
 <?php wp_enqueue_script('jquery'); ?>
 <?php wp_enqueue_script('cooquery', get_bloginfo('template_url').'/jquery.cooquery.js', array('jquery')); ?>
 <?php wp_enqueue_script('plaintext', get_bloginfo('template_url').'/plaintext.js', array('jquery')); ?>
