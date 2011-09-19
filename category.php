7ca4e5a42c152d6f0fd46e2dfcf897b17c19395d
Noah Liebman <noah@noahliebman.com>
2011-09-19 18:13:20 -0500

    Page 2 seems to work.

--- category.php
+++ category.php
@@ -7,28 +7,27 @@
  * @since PlainText 1.0
  */
 
+
 get_header(); ?>
+<body>
+	<!--[if lt IE 8]> <div style=' clear: both; height: 59px; padding:0 0 0 15px; position: relative;'> <a href="http://http://www.mozilla.com/en-US/firefox/new/?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div> <![endif]-->
+	<div class="container_12">
+		
+		<?php
+			/* Run the loop to output the posts.
+			 * If you want to overload this in a child theme then include a file
+			 * called loop-index.php and that will be used instead.
+			 */
+			 get_template_part( 'loop', 'category' );
+			?>
+	</div> <!-- end container_12 -->
+	<?php
+		/* Always have wp_footer() just before the closing </body>
+		 * tag of your theme, or you will break many plugins, which
+		 * generally use this hook to reference JavaScript files.
+		 */
 
-		<div id="container">
-			<div id="content" role="main">
-
-				<h1 class="page-title"><?php
-					printf( __( 'Category Archives: %s', 'plaintext' ), '<span>' . single_cat_title( '', false ) . '</span>' );
-				?></h1>
-				<?php
-					$category_description = category_description();
-					if ( ! empty( $category_description ) )
-						echo '<div class="archive-meta">' . $category_description . '</div>';
-
-				/* Run the loop for the category page to output the posts.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-category.php and that will be used instead.
-				 */
-				get_template_part( 'loop', 'category' );
-				?>
-
-			</div><!-- #content -->
-		</div><!-- #container -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+		wp_footer();
+	?>
+</body>
+</html>
