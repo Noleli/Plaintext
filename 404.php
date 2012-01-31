57da9c25dec6ca7f753e1d4d3a47d2334fe86491
Noah Liebman <noah@noahliebman.com>
2012-01-31 17:58:12 -0600

    Fixed the 404 page. I really need to figure out a better way to do that so there's no code duplication

--- 404.php
+++ 404.php
@@ -15,44 +15,42 @@
 
 get_header(); ?>
 <body>
-	<noscript>
-		<style type="text/css">
-			#sidebar {
-		        filter:alpha(opacity=100);
-		        -moz-opacity:1;
-		        -khtml-opacity: 1;
-		        opacity: 1;
-			}
-		</style>
-	</noscript>
 	<!--[if lt IE 8]> <div style=' clear: both; height: 59px; padding:0 0 0 15px; position: relative;'> <a href="http://http://www.mozilla.com/en-US/firefox/new/?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div> <![endif]-->
 	<div class="container_12">
-		<div class="grid_3 push_9">
-			<div id="sidebar">
-				<?php dynamic_sidebar('sidebar'); ?>
-				<div id="footer">
-					<p><a href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/" alt="Creative Commons Attribution-NonCommercial-ShareAlike"><img src="<?php bloginfo('template_directory'); ?>/images/cc-by-nc-sa.png"></a></p>
-					<p>Inspired by <a href="http://www.hogbaysoftware.com/products/plaintext">PlainText</a></p>
-				</div>
-			</div><!-- end sidebar -->
+		<?php
+/**
+ * The loop that displays posts.
+ *
+ * The loop displays the posts and the post content.  See
+ * http://codex.wordpress.org/The_Loop to understand it and
+ * http://codex.wordpress.org/Template_Tags to understand
+ * the tags used in it.
+ *
+ * This can be overridden in child themes with loop.php or
+ * loop-template.php, where 'template' is the loop context
+ * requested by a template. For example, loop-index.php would
+ * be used if it exists and we ask for the loop with:
+ * <code>get_template_part( 'loop', 'index' );</code>
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+?>
+
+<?php /* Display navigation to next/previous pages when applicable */ ?>
+
+<div class="grid_3 push_9">
+			<?php get_sidebar(); ?>
 		</div><!-- end grid_3 -->
 		<div class="grid_8 suffix_1 pull_3">
 			<div id="content_area">
 				<div id="head">
 					<h1><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<ul id="parent_pages" class="pages">
-						<?php $is_page = is_page(); ?>
-						<li<?php if(!$is_page) echo ' class="current_page_item"'; ?>><a href="<?php echo home_url(); ?>">Blog</a></li>
-						<?php wp_list_pages(array("depth"=>1, "title_li"=>"")); ?>
-					</ul>
 					
 					<?php
-						if($post->ancestors)
-						{
-							echo('<ul id="child_pages" class="pages">');
-							$children = wp_list_pages("title_li=&child_of=".end($post->ancestors));
-							echo('</ul>');
-						}
+						wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary', 'depth' => 1, 'menu' => 'main-nav' ) );
+						wp_nav_menu( array( 'container_class' => 'submenu-header', 'theme_location' => 'primary', 'depth' => 0, 'menu' => 'main-nav', 'walker' => new Custom_Walker_Nav_Sub_Menu() ) );
 					?>
 					
 				</div><!-- end head -->
@@ -61,10 +59,14 @@ get_header(); ?>
 					<div id="error">
 						<h1>404‽</h1>
 						<p>This ark has sailed. Find a better page to look at</p>
+					</div><!-- end error -->
+					<div id="bottom_nav">
+						<div class="bottom_dot">■</div>
 					</div>
 				</div><!-- end content -->
 			</div> <!-- end content_container -->
 		</div><!-- end grid_8 -->
+
 	</div> <!-- end container_12 -->
 	<?php
 		/* Always have wp_footer() just before the closing </body>
